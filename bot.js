f0d905a0b4bd43792681022f3af5dc55e651123d
js-all <titouangoulois29@gmail.com>
2021-04-22 21:32:55 +0200

    Fixes #14 add an **untested** implementation of the config prefix

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 
 // create the client
-var bot = new Discord.Client();
+const bot = new Discord.Client();
 
 // type info
 console.log("my github : https://github.com/yannis-mlgrn/bot-lph/ \nmade by yannis-mlglrn")
@@ -28,15 +28,17 @@ bot.on("message", message => {
     // when message begin by a !
     //if (message.content.indexOf("!") !== 0) return;
 
+    const usedPrefix = message.content[0];
     // cut the command for take all arguments
-    const args = message.content.slice(config.prefix).trim().split(/ +/s);
-     // take the command 
-    const command = args.shift().toLowerCase()
+    const args = message.content.substring(1, message.content.length).trim().split(/ +/s);
+    // take the command 
+    // kinda weird (and somewhat unoptimized) implementation, but is needed in order for prefixless commands to work
+    const command = usedPrefix === config.prefix ? args.shift().toLowerCase() : args.shift().substring(0, 0); // this thing is needed to perform the shift anyways and still return a string
     // take the username of message author
     const author = message.author.username;
     const authorId = message.author;
 
-    if (command == '!atelier') {
+    if (command == 'atelier') {
         // send message with all important link
         //message.channel.send("\n lien visio : https://mdl29.net/visio \nlien framapad : https://annuel.framapad.org/p/LPH");
         var msgatelier = new Discord.MessageEmbed()
@@ -63,7 +65,7 @@ bot.on("message", message => {
         message.channel.send(msgavatar);
         console.log("[*] "+author+" send !avatar command");
     }
-    if (command == '!help') {
+    if (command == 'help') {
         // send all command and explain how to use it
         //message.channel.send("les commandes :\n- !atelier : renvoi les liens importants (visio,pad...)\n- !random max : renvoi un nombre aléatoir entre 1 et le nombre inscrit\n- 
         //!avatar : renvoi l'image de ton avatar\n- !ping : le bot te répond pong\n- !yesno : créer un sondage de type yesno, ne pas oublier de mettre la question entre deux doubles cote\n- !activity : change l'activitée du bot, ne pas oublier de mettre l'activitée entre les deux doubles cote\n");
@@ -76,7 +78,7 @@ bot.on("message", message => {
         console.log("[*] "+author+" send !help command");
     }
     // random command 
-    if (command === "!random") {
+    if (command === "random") {
         let i = args[0]; // get the first argument  
         const random = Math.floor((Math.random() * i) + 1); // random command and return a int
         //message.reply('le résultat est : '+random);
@@ -91,7 +93,7 @@ bot.on("message", message => {
     if (message.content.match(/quoi\s*[?!.,]*\s*$/i)) { // if the message finish by quoi the bot reply "feur !". I use a regular expression
         message.channel.send("feur !"); // then send "feur !"
     }
-    if (command === "!yesno") {
+    if (command === "yesno") {
         // er --> "\"(.+?)\""g
         const mess = message.content.slice(config.prefix).trim();
         const q = mess.match(/"(.+?)"/g).map(v => v.replace(/^"(.+)"$/, "$1"));// get the content between "" 
@@ -117,7 +119,7 @@ bot.on("message", message => {
         });
         console.log("[*] "+author+" send !yesno command");
       }
-      if (command === "!activity") {
+      if (command === "activity") {
         // er --> "\"(.+?)\""g
         const mess = message.content.slice(config.prefix).trim();
         const z = mess.match(/"(.+?)"/g).map(v => v.replace(/^"(.+)"$/, "$1"));// get the content between "" 
